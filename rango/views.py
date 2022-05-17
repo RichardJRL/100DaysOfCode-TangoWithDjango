a6efa6c2280c5cf45bce89d04d0cb49235a1c2dc
Richard Leszczynski <59875350+RichardJRL@users.noreply.github.com>
2022-05-17 21:21:49 +0100

    completed Chapter 3 exercises to add an about view and hyperlinks in the HttpResponse objects

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Rango says hey there partner! For more information about Rango please click <a href='/rango/about/'>here</a>")
+
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page. To get back to the Rango homepage, click  <a href='/rango/'>here</a>")
