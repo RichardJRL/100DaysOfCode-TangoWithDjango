4d8a30e88cb597bc891907b1f14535337e24730f
Richard Leszczynski <59875350+RichardJRL@users.noreply.github.com>
2022-05-18 23:56:45 +0100

    Add static resources directory, a test image and update settings.py for static resources

--- tangowithdjango/settings.py
+++ tangowithdjango/settings.py
@@ -18,6 +18,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -121,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
